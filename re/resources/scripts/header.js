ca430c16985b2a512480364b1556d92330f83def
fcu-d0486228 <d0486228@fcu.edu.tw>
2017-12-27 07:11:09 +0800

    Add files via upload

---
+++ re/resources/scripts/header.js
@@ -0,0 +1,13 @@
+header = '<div id="header">\
+      <nav class="navbar navbar-expand-sm fixed-top navbar-light fixednav" id="nav-header">\
+      <a class="navbar navbar-brand" href="../../home.html">CTGO</a>\
+        <ul class="navbar-nav">\
+          <li class="nav-item"><a class="nav-link" href="../../族群與文化.html">族權與文化</a></li>\
+          <li class="nav-item"><a class="nav-link" href="%E5%95%86%E5%93%81.html">商品</a></li>\
+          <li class="nav-item"><a class="nav-link" href="%E6%9C%8D%E5%8B%99%E6%8F%90%E4%BE%9B.html">旅遊服務提供</a></li>\
+          <li class="nav-item"><a class="nav-link" href="%E9%81%8A%E5%AE%A2.html">遊客意願</a></li>\
+          <li class="nav-item"><a class="nav-link" href="%E7%AF%80%E6%97%A5%E8%88%87%E8%AA%9E%E8%A8%80%E5%AD%B8%E7%BF%92.html">節日與語言學習</a></li>\
+        </ul><a class="btn btn-outline-success text-dark ml-md-auto" href="%E7%99%BB%E5%85%A5.html">登入</a>\
+      </nav>\
+    </div>';
+document.write(header);
