2d6c615905b362dc9e793351b11839baeec9b96f
owl OH <d0440478@fcu.edu.tw>
2018-01-06 02:33:56 +0800

    New navbar for responsive web design

--- web_project/common/resources/scripts/header.js
+++ web_project/common/resources/scripts/header.js
@@ -1,6 +1,10 @@
 header = '<div id="header">\
-			<nav class="navbar navbar-expand-sm fixed-top navbar-light fixednav" id="nav-header">\
+			<nav class="navbar navbar-expand-sm fixed-top navbar-light fixednav fixed-top" id="nav-header">\
 				<a class="navbar navbar-brand" href="../common/index.html">CTGO</a>\
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">\
+				<span class="navbar-toggler-icon"></span>\
+			  	</button>\
+				<div class="collapse navbar-collapse" id="collapsibleNavbar">\
 				<ul class="navbar-nav">\
 					<li class="nav-item">\
 						<a class="nav-link" href="../culture/culture_main.html">族權與文化</a>\
@@ -19,6 +23,7 @@ header = '<div id="header">\
 					</li>\
 				</ul>\
 				<a class="btn btn-outline-success text-dark ml-md-auto" href="../login/login.html">登入</a>\
+				</div>\
 			</nav>\
 		</div>';
 document.write(header);
